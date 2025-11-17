3717f7326b125b773bcd5a0a3210a960828965c4
Matthew Blacklock <matthewblacklock@gmail.com>
2025-11-17 23:55:40 +0000

    added static_root to base settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,5 +7,3 @@ ALLOWED_HOSTS = ['mblacklock.pythonanywhere.com']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@4egzz4rm--gqesxb#f013#7(h!2t226s(-70f5y0vxj$fmths'
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
