ad8805ad8f07eebda5909dfe80f145db3bfcaea5
Matthew Blacklock <matthewblacklock@gmail.com>
2026-05-28 13:27:31 +0100

    renamed confirm urls to mapping

--- assessment_feedback/urls.py
+++ assessment_feedback/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.upload_file, name="upload_file"),
-    path("confirm/", views.confirm_mappings, name="confirm_mappings"),
+    path("mapping/", views.confirm_mappings, name="confirm_mappings"),
     path("layout/", views.configure_layout, name="configure_layout"),
     path("process/", views.process_feedback, name="process_feedback"),
     path("rubric-bands/", views.rubric_bands_api, name="rubric_bands_api"),
